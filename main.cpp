78a2b3954e5ce895ed9088fa622e9cdf6acf9afb
sookki9205 <sookki9205@gmail.com>
2023-02-09 13:18:18 -0700

    Version - main : <return value>

--- main.cpp
+++ main.cpp
@@ -1,4 +1,4 @@
-#include <iostream>
+#include <iostream
 #include <vector>
 
 // returns a vector with true for numbers less than the second parameters and false for those greater than or equal to
@@ -10,6 +10,6 @@ std::vector<bool> GreaterMask(std::vector<int> nums, int greater_than);
 int main(){
     //call each of the above functions
 
-    return 2;
+    return 4;
 
 }
